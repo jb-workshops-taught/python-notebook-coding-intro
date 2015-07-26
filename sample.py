7aebdcd4008977299f9cc8c9408a14da4ef7ced7
Jenny Brown <jennybrown8@gmail.com>
2015-07-26 12:07:46 -0500

    Initial stub build

---
+++ sample.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("Hello world!")
+
+
+# In[3]:
+
+print("I can do math.  Want to see?")
+answer = 5 + 3
+print("Here's the answer! ")
+print(answer)
+
+
+# In[ ]:
+
+
+
